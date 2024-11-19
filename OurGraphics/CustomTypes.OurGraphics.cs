6352d08f8fe3452e0399c718d71d7b47d75fa412
K-Georgei <91503291+K-Georgei@users.noreply.github.com>
2024-11-19 14:30:11 +0100

    Matrix4 type and operators done

--- OurGraphics/CustomTypes.OurGraphics.cs
+++ OurGraphics/CustomTypes.OurGraphics.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -175,17 +176,120 @@ namespace OurGraphics
             #endregion
         }
 
-        public class Matrix
+        public class Matrix4
         {
-            private int M { get; set; }
-            private int N { get; set; }
+            private double[,] Values;
 
-
-            public Matrix(int m, int n) 
+            public Matrix4() 
             {
-                M = m;
-                N = n;
+                this.Values = new double[4,4];
+                for (int i = 0; i < 4; i++)
+                {
+                    for(int j = 0; j < 4; j++)
+                    {
+                        this[i, j] = 0;
+                    }
+                }
             }
+
+            public Matrix4(Matrix4 matrix)
+            {
+                this.Values = new double[4, 4];
+
+                for (int i = 0; i < Values.GetLength(0); i++)
+                {
+                    for (int j = 0; j < Values.GetLength(0); j++)
+                    {
+                        this[i, j] = matrix[i,j];
+                    }
+                }
+            }
+
+            public void IdentityMatrix()
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        if (i == j)
+                        {
+                            this[i, j] = 1;
+
+                        }
+                        else
+                        {
+                            this[i, j] = 0;
+                        }
+                    }
+                }
+            }
+
+
+            public double this[int row, int col]
+            {
+                get { return Values[row, col]; }
+                set { Values[row, col] = value; }
+            }
+
+            public static Matrix4 operator +(Matrix4 a, Matrix4 b)
+            {
+                Matrix4 result = new Matrix4();
+                for(int i = 0;i < 4; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        result[i,j] = a[i,j] + b[i,j];   
+                    }
+                }
+                return result;
+            }
+
+            public static Matrix4 operator *(Matrix4 a, Matrix4 b)
+            {
+                Matrix4 result = new Matrix4();
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        double temp = 0;
+                        for (int k = 0; k < 4; k++)
+                        {
+                            temp += a[i, k] * b[k, j];
+
+                        }
+                        result[i,j] = temp;
+                    }
+                }
+                return result;
+            }
+
+            public static Matrix4 operator *(Matrix4 a, double scalar)
+            {
+
+                Matrix4 result = new Matrix4();
+
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        result[i, j] = a[i, j] * scalar;
+                    }
+                }
+
+                return result;
+            }
+
+            public static Vector4 operator *(Matrix4 m, Vector4 v)
+            {
+                Vector4 res = new Vector4(0, 0, 0, 1);
+                res.X = m[0, 0] * v.X + m[0, 1] * v.Y + m[0, 2] * v.Z + m[0, 3] * v.W;
+                res.Y = m[1, 0] * v.X + m[1, 1] * v.Y + m[1, 2] * v.Z + m[1, 3] * v.W;
+                res.Z = m[2, 0] * v.X + m[2, 1] * v.Y + m[2, 2] * v.Z + m[2, 3] * v.W;
+                res.W = m[3, 0] * v.X + m[3, 1] * v.Y + m[3, 2] * v.Z + m[3, 3] * v.W;
+                return res;
+            }
+
+
         }
 
     }
